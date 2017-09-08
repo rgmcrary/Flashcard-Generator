8d67c1e9b70cf42216474726d02648b8792a5df3
Rich McRary <rgmcrary@gmail.com>
2017-09-08 18:03:42 -0400

    Constructor modules created

--- BasicCard.js
+++ BasicCard.js
@@ -1,1 +1,31 @@
+var BasicCard = function(front, back) {
+    if (!(this instanceof BasicCard)) { 
+        return new BasicCard(front, back);
+      }
+    this.front = front;
+    this.back = back;
+};
+
+
+var cards = [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = BasicCard;
+
+
+
+
+
 
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,1 +1,14 @@
+var ClozeCard = function(text, cloze) {
+    if (!(this instanceof ClozeCard)) { 
+        return new ClozeCard(text, cloze);
+      }
+    this.fullText = text;
+  if (text.indexOf(cloze) !== -1) {
+    this.partialText = text.replace(cloze, "...");
+  } else {
+    console.log("This text doesn't include the cloze value you entered.");
+  }
+  this.cloze = cloze;
+};
 
+module.exports = ClozeCard;
