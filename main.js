9fbd7de1f60c706f51da6e33da7ef2c95cb621dd
Rich McRary <rgmcrary@gmail.com>
2017-09-11 19:15:46 -0400

    Added command line interface for creation of cards

--- main.js
+++ main.js
@@ -1,16 +1,60 @@
-var BasicCard = require ("./BasicCard.js");
-var ClozeCard = require ("./ClozeCard.js");
+var BasicCard = require("./BasicCard.js");
+var ClozeCard = require("./ClozeCard.js");
+var inquirer = require("inquirer");
 
-var firstPresident = BasicCard(
-    "Who was the first president of the United States?", "George Washington");
 
-    console.log(firstPresident.front); 
-    console.log(firstPresident.back);   
-    
-    
-    var firstPresidentCloze = ClozeCard(
-        "George Washington was the first president of the United States.", "George Washington");
-    
-    console.log(firstPresidentCloze.cloze);
-    console.log(firstPresidentCloze.fullText);
-    console.log(firstPresidentCloze.partialText);
+// Function option call
+inquirer
+  .prompt([
+    {
+      type: "checkbox",
+      name: "cardType",
+      message: "What type of card do you want to create??",
+      choices: ["BasicCard", "ClozeCard"]
+    }
+  ])
+  .then(function(answer) {
+    console.log(answer.cardType)
+    // BasicCard creation
+    if (answer.cardType[0] === "BasicCard") {
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "front",
+            message: "Please input the question."
+          },
+          {
+            type: "input",
+            name: "back",
+            message: "Please input the answer."
+          }
+        ])
+        .then(function(basicAns) {
+          var basicCard = BasicCard(basicAns.front, basicAns.back);
+          console.log(basicCard.front);
+          console.log(basicCard.back);
+        });
+    } else if (answer.cardType[0] === "ClozeCard") {
+      // ClozeCard creation
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "text",
+            message: "Please input the full sentence."
+          },
+          {
+            type: "input",
+            name: "cloze",
+            message: "Please input the cloze."
+          }
+        ])
+        .then(function(clozeAns) {
+          var clozeCard = ClozeCard(clozeAns.text, clozeAns.cloze);
+          console.log(clozeCard.fullText);
+          console.log(clozeCard.partialText);
+          console.log(clozeCard.cloze);
+        });
+    }
+  });
